5d621042ad02256c21200690d9485114d345d2fc
reznichenkosa <sergiliocr@gmail.com>
2022-02-04 15:27:18 +0300

    add Tabs for site

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const tabs = document.querySelectorAll('.tabheader__item');
+    const tabContent = document.querySelectorAll('.tabcontent');
+    const tabHeader = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+        tabContent.forEach(item => {
+            item.classList.remove('visible' ,'fadeIn');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabContent[i].classList.add('visible', 'fadeIn');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabHeader.addEventListener('click', (e) => {
+        if (e.target && e.target.matches('.tabheader__item')) {
+            const tab = e.target;
+            tabs.forEach((item, i) => {
+                if (tab === item) {
+                    hideTabContent();
+                    showTabContent(i);
+                } 
+            });
+
+        }
+    });
+});
+
+
