67ff5175b588235ec6f04498ff753882b12e2b89
reznichenkosa <sergiliocr@gmail.com>
2022-02-10 19:42:35 +0300

    add calc for calories section

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
             item.classList.remove('tabheader__item_active');
         });
         tabContent.forEach(item => {
-            item.classList.remove('visible' ,'fadeIn');
+            item.classList.remove('visible', 'fadeIn');
         });
     }
 
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (tab === item) {
                     hideTabContent();
                     showTabContent(i);
-                } 
+                }
             });
 
         }
@@ -39,19 +39,19 @@ document.addEventListener('DOMContentLoaded', () => {
     // timer 
 
     const deadline = '2022-03-07 00:00',
-          promoDescr = document.querySelector('.promotion__descr'),
-          month = ['января','февраля','марта','апреля','мая','июня','июля','августа','сентября','октября','ноября','декабря'],
-          deadlineDate = new Date(deadline);
-    
+        promoDescr = document.querySelector('.promotion__descr'),
+        month = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля', 'августа', 'сентября', 'октября', 'ноября', 'декабря'],
+        deadlineDate = new Date(deadline);
+
     promoDescr.innerHTML += `Акция закончится ${deadlineDate.getDate()} ${month[deadlineDate.getMonth()]} в ${deadline.slice(-5)}`;
 
     function getTimeRemainig(endTime) {
         const time = new Date(endTime) - new Date(),
-              days = Math.floor(time / 1000 / 60 / 60 / 24),
-              hours = Math.floor(time / 1000 / 60 / 60 % 24),
-              minutes = Math.floor(time / 1000 / 60 % 60),
-              seconds = Math.floor(time / 1000 % 60);
-        
+            days = Math.floor(time / 1000 / 60 / 60 / 24),
+            hours = Math.floor(time / 1000 / 60 / 60 % 24),
+            minutes = Math.floor(time / 1000 / 60 % 60),
+            seconds = Math.floor(time / 1000 % 60);
+
         return {
             time,
             days,
@@ -63,19 +63,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function showTime(time) {
         if (time >= 0 && time < 10) {
-            return '0'+time;
+            return '0' + time;
         }
         return time
     }
 
     function setTimePromo(deadline) {
         const days = document.querySelector('#days'),
-              hours = document.querySelector('#hours'),
-              minutes = document.querySelector('#minutes'),
-              seconds = document.querySelector('#seconds'),
-              timer = setInterval(updateTimer, 1000);
+            hours = document.querySelector('#hours'),
+            minutes = document.querySelector('#minutes'),
+            seconds = document.querySelector('#seconds'),
+            timer = setInterval(updateTimer, 1000);
 
         updateTimer();
+
         function updateTimer() {
             const time = getTimeRemainig(deadline);
 
@@ -83,7 +84,7 @@ document.addEventListener('DOMContentLoaded', () => {
             hours.textContent = showTime(time.hours);
             minutes.textContent = showTime(time.minutes);
             seconds.textContent = showTime(time.seconds);
-            
+
             if (time.time <= 1000) {
                 clearInterval(timer);
                 days.textContent = '00';
@@ -93,19 +94,19 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.querySelector('.promotion').style.display = 'none';
             }
         }
-    }          
-    
+    }
+
     setTimePromo(deadline);
 
     // modal 
 
     const modal = document.querySelector('.modal'),
-          btns = document.querySelectorAll('[data-modal]');
-    
+        btns = document.querySelectorAll('[data-modal]');
+
     btns.forEach(btn => {
         btn.addEventListener('click', openModal);
     });
-    
+
     function openModal() {
         modal.style.display = 'block';
         modal.classList.add('fadeInModal');
@@ -122,7 +123,7 @@ document.addEventListener('DOMContentLoaded', () => {
         }, 700);
         document.body.style.overflow = 'auto';
     }
-    
+
 
     modal.addEventListener('click', (e) => {
         if (e.target && e.target.matches('.modal') || e.target.getAttribute('data-close') == '') {
@@ -183,20 +184,28 @@ document.addEventListener('DOMContentLoaded', () => {
         if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
-        return await res.json(); 
+        return await res.json();
     };
 
     getResource('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({img, altimg, title, descr, price}) => {
+            data.forEach(({
+                img,
+                altimg,
+                title,
+                descr,
+                price
+            }) => {
                 new MenuCard('.menu__field .container', img, altimg, title, descr, price).showCard();
             });
         });
 
-    
+
     // forms
 
     const forms = document.querySelectorAll('form');
+
+   
 
     const message = {
         loading: 'icons/spinner.svg',
@@ -219,7 +228,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
         return await res.json();
     };
-
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
@@ -234,15 +242,15 @@ document.addEventListener('DOMContentLoaded', () => {
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
 
             postData('http://localhost:3000/requests', json)
-            .then(data => {
-                console.log(data);
-                showThanksModal(message.success);
-                statusMessage.remove();
-            }).catch(() => {
-                showThanksModal(message.failure);
-            }).finally(() => {
-                form.reset();
-            })
+                .then(data => {
+                    console.log(data);
+                    showThanksModal(message.success);
+                    statusMessage.remove();
+                }).catch(() => {
+                    showThanksModal(message.failure);
+                }).finally(() => {
+                    form.reset();
+                })
 
         });
     }
@@ -254,7 +262,7 @@ document.addEventListener('DOMContentLoaded', () => {
             modalDialog.style.display = 'none';
             modalDialog.classList.remove('fadeOutModal');
             openModal();
-        
+
 
             const thanksModal = document.createElement('div');
             thanksModal.classList.add('modal__dialog', 'fadeInModal');
@@ -274,36 +282,42 @@ document.addEventListener('DOMContentLoaded', () => {
             }, 4000);
         }, 700);
 
-        
+
     }
 
 
     // slider 
 
     const prev = document.querySelector('.offer__slider-prev'),
-          next = document.querySelector('.offer__slider-next'),
-          current = document.querySelector('#current'),
-          total = document.querySelector('#total'),
-          slides = document.querySelectorAll('.offer__slide'),
-          boxSlider = document.querySelector('.offer__slider-wrapper');
+        next = document.querySelector('.offer__slider-next'),
+        current = document.querySelector('#current'),
+        total = document.querySelector('#total'),
+        slides = document.querySelectorAll('.offer__slide'),
+        boxSlider = document.querySelector('.offer__slider-wrapper');
 
     const sliderNav = document.createElement('ul');
-          sliderNav.classList.add('offer__slider-nav');
-          sliderNav.innerHTML = `
+    sliderNav.classList.add('offer__slider-nav');
+    sliderNav.innerHTML = `
           <li></li>
           <li></li>
           <li></li>
           <li></li>`;
-      
+
     boxSlider.insertAdjacentElement('afterend', sliderNav);
-      
+
     const sliderNavItems = document.querySelectorAll('.offer__slider-nav li');
-    
+
     function showSlide(i) {
-        boxSlider.style.left = -i*(Number.parseInt(window.getComputedStyle(slides[i]).width)) + 'px';
-        current.textContent = addZeroCount(i+1);
+        if (i < 0) {
+            i = slides.length - 1;
+        }
+        if (i >= slides.length) {
+            i = 0;
+        }
+        boxSlider.style.left = -i * (Number.parseInt(window.getComputedStyle(slides[i]).width)) + 'px';
+        current.textContent = addZeroCount(i + 1);
         total.textContent = addZeroCount(slides.length);
-        
+
         sliderNavItems.forEach((item, index) => {
             if (index === i) {
                 sliderNavItems[index].style.background = '#54ed39';
@@ -323,11 +337,26 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     showSlide(0);
 
+    //autoplay
+
+    function autoplaySlider() {
+        let currenSlide = +current.textContent;
+        currenSlide = (currenSlide >= slides.length) ? 0 : currenSlide;
+        current.classList.add('scaleNumber');
+        setTimeout(() => {
+            current.classList.remove('scaleNumber');
+        }, 200);
+        showSlide(currenSlide);
+    }
+    const autoplayId = setInterval(autoplaySlider, 4000);
+
+
+
     next.addEventListener('click', () => {
+        clearInterval(autoplayId);
+
         let count = +current.textContent;
-        if (count >= slides.length) {
-            count = 0;
-        }
+
         showSlide(count);
 
         current.classList.add('scaleNumber');
@@ -335,23 +364,91 @@ document.addEventListener('DOMContentLoaded', () => {
             current.classList.remove('scaleNumber');
         }, 200);
     });
+
     prev.addEventListener('click', () => {
-        let count = +current.textContent-2;
-        if (count < 0) {
-            count = slides.length - 1;
-        }
+        clearInterval(autoplayId);
+        let count = +current.textContent - 2;
+
         showSlide(count);
         current.classList.add('scaleNumber');
         setTimeout(() => {
             current.classList.remove('scaleNumber');
         }, 200);
     })
-    
-    
-    
+
     sliderNavItems.forEach((item, i) => {
         item.addEventListener('click', () => {
             showSlide(i);
         })
     });
+
+    // calculator 
+
+    const result = document.querySelector('.calculating__result span');
+    let sex, height, weight, age, ratio;
+
+    function calcTotal() {
+        if (!(sex && height && weight && age && ratio)) {
+            result.textContent = '_____';
+            return;
+        }
+
+        if(sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+
+        if (result.textContent <= 0) {
+            result.textContent = '____';
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInfo(parentSelector, activeClass) {
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        document.querySelector(parentSelector).addEventListener('click', (e) => {
+            
+            if (e.target != document.querySelector(parentSelector)) {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio')
+                } else {
+                    sex = e.target.getAttribute('data-sex')
+                }
+    
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                })
+                
+                e.target.classList.add(activeClass);
+            }
+            calcTotal();
+        });
+    }
+
+    getStaticInfo('#gender', 'calculating__choose-item_active');
+    getStaticInfo('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInfo(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+            if (input.value.match(/\D/g)) {
+                input.style.border = '2px solid red';
+            } else {
+                input.style.border = 'none';
+            }
+            age = (input.getAttribute('id') === 'age') ? +input.value : age;
+            weight = (input.getAttribute('id') === 'weight') ? +input.value : weight;
+            height = (input.getAttribute('id') === 'height') ? +input.value : height; 
+            calcTotal();
+        });
+        
+    }
+
+    getDynamicInfo('#height');
+    getDynamicInfo('#weight');
+    getDynamicInfo('#age');
 });
